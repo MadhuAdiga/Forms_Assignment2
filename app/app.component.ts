6cb6ff75a92e0ab1f3b6985a7cd2fdc18b9fdc7a
MadhuAdiga <madhu.vah@gmail.com>
2017-05-27 16:08:21 +0530

    Forms_Assignment2

---
+++ app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'; // We will need to import a couple of specific API’s for dealing with reactive forms
+import { FormsModule } from '@angular/forms';
+
+
+@Component({
+  selector: 'my-app',
+  templateUrl: `./app/app.component.html`,
+   styles: [`
+            input.ng-invalid {border-left:5px solid red;} 
+            input.ng-valid {border-left:5px solid green;}
+            `]
+})// Apply different styles to the input field when it is invalid and valid
+
+
+export class AppComponent  { // Create a class AppComponent with app.component.html as templateUrl
+
+ onSubmit(value:any){
+        console.log(value);
+    }
+} // Logs valid input values to the console on submit
+
+    
+    
+
